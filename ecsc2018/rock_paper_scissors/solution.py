ab88ddceb76ef624049873335db55ba809de299c
Michał Hryb <mhryb@blastlab.co>
2019-09-25 13:22:41 +0200

    squish solution into one file

--- ecsc2018/rock_paper_scissors/solution.py
+++ ecsc2018/rock_paper_scissors/solution.py
@@ -6,7 +6,7 @@ import random
 
 winning_moves = []
 orig_moves = []
-moves = ['8<', 'O', '__']
+moves = ['O', '__', '8<']
 
 def read_response(sock):
 	ready = select.select([sock], [], [], 0.05)
@@ -22,8 +22,8 @@ def read_response(sock):
 
 def winning_move(move):
 	win_moves = {
-	"_" : "8<",
-	"8" : "O",
+	"__" : "8<",
+	"8<" : "O",
 	"O" : "__"
 	}
 	return win_moves[move]
@@ -34,7 +34,7 @@ def next_move(iter):
 	else:
 		return moves[random.randint(0,2)] + '\n'
 
-
+resp = ''
 max_iter = 0;
 while (len(winning_moves) <= 50):
 	iter = 0
@@ -45,21 +45,24 @@ while (len(winning_moves) <= 50):
 	for i in range (0, max_iter+1):
 		sock.send(str.encode(next_move(iter)))
 		resp = read_response(sock)
-		print(resp)
 		
 		if(resp.find('cheated') != -1):
-			print('found!')
 			sock.send(str.encode('313487\n'))
-			resp2 = read_response(sock)
-			print(resp2)
+			resp = read_response(sock)
+			#print(resp)
 			break
 
 		substr = resp.find('My move:')
 		if (substr >= 0):
 			iter = iter + 1
-			move = resp[substr+9]
-			print('move = ' + move)
+			if (resp[substr+9] == 'O'):
+				move = resp[substr+9]
+			else:
+				move = resp[substr+9:substr+11]
+
 			if (iter > max_iter): #unikalny iterator
+				print('iteration #' + str(iter))
+				print('move = ' + move)
 				max_iter = iter
 				winning_moves.append(winning_move(move))
 				orig_moves.append(move)
@@ -70,12 +73,50 @@ while (len(winning_moves) <= 50):
 	if (len(winning_moves) == 50):
 		break
 
-print('win_moves')
+
+print(resp)
+print('winning moves:')
 print(winning_moves)
-print('orig_moves')
+print('original moves: ')
 print(orig_moves)
 
+moves_int = []
 
-sock.close()
+for i in orig_moves:
+	moves_int.append(moves.index(i))
+print(moves_int)
+
+seeds = {}
+for s in range(0,65536):
+	random.seed(s)
+	ctr = 0
+	for i in moves_int:
+		move = random.randint(0, 2)
+		if (i != move):
+			break
+		ctr += 1
+		if (ctr == 50):
+			print('suspected seed: ' + str(s))
+			seeds[str(s)] = str(random.randint(0, 10**6))
+			print('Random number: ' + seeds[str(s)])
+
+for seed_key in seeds.keys():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect(('ecsc18.hack.cert.pl', 10021))
+	print(read_response(sock))	#welcome message
+
+	for i in winning_moves:
+		sock.send(str.encode(i + '\n'))
+		resp = read_response(sock)
+		print(resp)
+
+		if(resp.find('cheated') != -1):
+			print('magic num: ' + str(seeds[seed_key]))
+			sock.send(str.encode(str(seeds[seed_key]) + '\n'))
+			resp = read_response(sock)
+			print(resp)
+			break
+	sock.close()
+
+
 print('done')
-
